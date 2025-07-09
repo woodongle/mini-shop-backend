6030d888363cbeb33e0f90f7bc08504bfe555286
woodongle <315ehdgur@gmail.com>
2025-07-09 15:21:05 +0900

    first commit

---
+++ src/main/java/mini/minishop/MinishopApplication.java
@@ -0,0 +1,12 @@
+package mini.minishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinishopApplication.class, args);
+	}
+}
