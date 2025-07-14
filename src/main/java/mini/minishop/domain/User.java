401281b41ae1984d80d9139317801cbe1ff7fe62
woodongle <315ehdgur@gmail.com>
2025-07-14 10:47:53 +0900

    feat: 컬럼 제약조건 추가
    1. Users 테이블의 email 컬럼에 unique 제약 조건 추가

--- src/main/java/mini/minishop/domain/User.java
+++ src/main/java/mini/minishop/domain/User.java
@@ -1,10 +1,13 @@
 package mini.minishop.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -18,7 +21,10 @@ public class User {
     private Long id;
 
     private String name;
+
+    @Column(unique = true)
     private String email;
+    
     private String password;
     private LocalDateTime createdDate;
 }
