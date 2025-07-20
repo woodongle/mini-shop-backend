63c85ac8ca78afdf81107dbeb44b0656d7639cc9
woodongle <315ehdgur@gmail.com>
2025-07-20 16:03:31 +0900

    style: config 패키지 생성
    1. config 파일이 여러 개 존재하여 유지보수를 위해 패키지에 같이 관리

--- src/main/java/mini/minishop/config/WebConfig.java
+++ src/main/java/mini/minishop/config/WebConfig.java
@@ -1,4 +1,4 @@
-package mini.minishop;
+package mini.minishop.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
