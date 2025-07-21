a7903e6da3dcba667b4ba6e823d3d03904148120
woodongle <315ehdgur@gmail.com>
2025-07-21 11:55:37 +0900

    feat: 도메인에 맞는 에러 코드 Enum 생성
    1. 도메인에 맞는 에러 코드를 관리하기 위해 Enum 생성
      - UserErrorCode

---
+++ src/main/java/mini/minishop/error/UserErrorCode.java
@@ -0,0 +1,20 @@
+package mini.minishop.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum UserErrorCode {
+    ALREADY_EXISTS_EMAIL("U001", "이미 존재하는 이메일입니다.", null),
+    USER_NOT_FOUND("U002", "존재하지 않는 회원입니다.", HttpStatus.NOT_FOUND);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus status;
+
+    UserErrorCode(String code, String message, HttpStatus status) {
+        this.code = code;
+        this.message = message;
+        this.status = status;
+    }
+}
