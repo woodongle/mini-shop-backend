be86d8a83209f51b28fa23d25b42c3ee1b2e49dc
woodongle <315ehdgur@gmail.com>
2025-07-27 17:07:53 +0900

    feat: Goods 엔티티와 User 매핑 추가
    1. 이전에는 상품을 등록하면 어떤 사용자가 등록했는지 알 수 없었기 때문에 사용자 정보를 넣기 위해 User와 매핑하는 코드 추가

--- src/main/java/mini/minishop/domain/Goods.java
+++ src/main/java/mini/minishop/domain/Goods.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -33,6 +35,10 @@ public class Goods extends BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Builder
     public Goods(String name, BigDecimal price, int inventoryQuantity) {
