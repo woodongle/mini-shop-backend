a3e82bded96b51f4da2d25c8c907fea317b109ba
woodongle <315ehdgur@gmail.com>
2025-07-28 20:07:39 +0900

    refactor: 메서드명 변경
    1. 메서드명을 통일시키기 위해 메서드명 변경

--- src/main/java/mini/minishop/controller/UserController.java
+++ src/main/java/mini/minishop/controller/UserController.java
@@ -49,7 +49,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
-    public ResponseEntity<FindUserResponse> getUserById(@PathVariable Long userId) {
+    public ResponseEntity<FindUserResponse> findUserById(@PathVariable Long userId) {
         Optional<FindUserResponse> findUser = userService.findUser(userId);
 
         return findUser.map(response -> new ResponseEntity<>(response, HttpStatus.OK))
