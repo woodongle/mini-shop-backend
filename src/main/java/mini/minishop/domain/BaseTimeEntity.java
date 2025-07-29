39afd61ccf74a82207809e5351a094b813baa9e0
woodongle <315ehdgur@gmail.com>
2025-07-29 15:55:16 +0900

    feat: 공통으로 사용하는 필드명 수정
    1. 명확한 뜻을 전달하기 위해 공통으로 사용하는 필드명 수정

--- src/main/java/mini/minishop/domain/BaseTimeEntity.java
+++ src/main/java/mini/minishop/domain/BaseTimeEntity.java
@@ -13,5 +13,5 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
 }
