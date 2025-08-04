4651d47ae45e4f348688f8ced0ed16389e24136b
woodongle <315ehdgur@gmail.com>
2025-08-04 21:28:51 +0900

    feat: 모든 엔티티의 createdDate 컬럼 규칙 추가
    1. 생성 날짜는 한 번 등록되면 수정되지 않게 규칙 추가

--- src/main/java/mini/minishop/domain/BaseTimeEntity.java
+++ src/main/java/mini/minishop/domain/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package mini.minishop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -13,5 +14,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 }
