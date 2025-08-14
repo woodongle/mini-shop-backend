a7fccedbb9211b774b47257a50dd4c6327804b03
woodongle <315ehdgur@gmail.com>
2025-08-14 21:28:01 +0900

    refactor: 예외 처리 로직 수정 및 ErrorResponse 필드 변경
    1. 확실한 의미를 전달하기 위해 ErrorResponse의 필드에 HttpStatus 추가 및 기존 status를 code로 수정
    2. 전역 예외 처리 핸들러에서 표준 응답 DTO를 사용하도록 변경

--- src/main/java/mini/minishop/api/ErrorResponse.java
+++ src/main/java/mini/minishop/api/ErrorResponse.java
@@ -3,14 +3,16 @@ package mini.minishop.api;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import mini.minishop.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class ErrorResponse {
-    private final int status;
+    private final int code;
+    private final HttpStatus status;
     private final String message;
 
     public static ErrorResponse of(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.getStatus().value(), errorCode.getMessage());
+        return new ErrorResponse(errorCode.getStatus().value(), errorCode.getStatus(), errorCode.getMessage());
     }
 }
--- src/main/java/mini/minishop/handler/GlobalExceptionHandler.java
+++ src/main/java/mini/minishop/handler/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package mini.minishop.handler;
 
+import mini.minishop.api.ApiResponse;
 import mini.minishop.api.ErrorResponse;
 import mini.minishop.exception.BusinessException;
 import mini.minishop.exception.CommonErrorCode;
 import mini.minishop.exception.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,18 +13,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+    protected ResponseEntity<ApiResponse<ErrorResponse>> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode);
 
-        return new ResponseEntity<>(response, errorCode.getStatus());
+        return new ResponseEntity<>(ApiResponse.of(response.getStatus(), response), errorCode.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+    protected ResponseEntity<ApiResponse<ErrorResponse>> handleException(Exception e) {
         ErrorResponse response = ErrorResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR);
 
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(ApiResponse.of(response.getStatus(), response), response.getStatus());
     }
 }
 
