231460f7631d345473ea34729a45d523fed5520f
woodongle <315ehdgur@gmail.com>
2025-08-16 15:48:37 +0900

    feat: 애노테이션 추가
    1. 의미를 확실하게 하기 위해 누락되어 있는 @Repository 애노테이션 추가

--- src/main/java/mini/minishop/domain/user/RefreshTokenRepository.java
+++ src/main/java/mini/minishop/domain/user/RefreshTokenRepository.java
@@ -2,7 +2,9 @@ package mini.minishop.domain.user;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
 
