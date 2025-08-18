835ebb16c88d44b27d26f24f81579a762c5b0066
woodongle <315ehdgur@gmail.com>
2025-08-18 13:58:35 +0900

    fix: DTO 필드 검증 메시지 수정
    1. 주문 생성 DTO의 필드 검증 메시지를 다른 검증 메시지와 동일하게 수정

--- src/main/java/mini/minishop/api/controller/order/request/CreateOrderRequest.java
+++ src/main/java/mini/minishop/api/controller/order/request/CreateOrderRequest.java
@@ -12,6 +12,8 @@ public class CreateOrderRequest {
     @NotBlank(message = "주소는 필수입니다.")
     private String address;
 
-    @Positive(message = "주문 상품 수량은 1 이상이어야 합니다.")
+    @Positive(message = "주문 상품 수량은 양수여야 합니다.")
     private int orderGoodsQuantity;
+
+
 }
