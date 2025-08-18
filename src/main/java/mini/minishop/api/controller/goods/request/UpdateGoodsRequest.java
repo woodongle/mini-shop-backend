5faacb4a4f5aa534363ca59cb2fe62b8ef5cbd39
woodongle <315ehdgur@gmail.com>
2025-08-18 12:29:13 +0900

    fix: 사용자 요청 DTO 필드의 검증 로직 변경
    1. 의미를 간단하고 명확하게 하기 위해 검증 로직 변경
      - @Min(최소 1 이상) -> @Positive(양수)

--- src/main/java/mini/minishop/api/controller/goods/request/UpdateGoodsRequest.java
+++ src/main/java/mini/minishop/api/controller/goods/request/UpdateGoodsRequest.java
@@ -1,8 +1,8 @@
 package mini.minishop.api.controller.goods.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Getter;
@@ -19,6 +19,6 @@ public class UpdateGoodsRequest {
     @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
     private BigDecimal price;
 
-    @Min(value = 1, message = "재고 수량은 1 이상이어야 합니다.")
+    @Positive(message = "재고 수량은 양수여야 합니다.")
     private int inventoryQuantity;
 }
--- src/main/java/mini/minishop/api/controller/order/request/CreateOrderRequest.java
+++ src/main/java/mini/minishop/api/controller/order/request/CreateOrderRequest.java
@@ -1,7 +1,7 @@
 package mini.minishop.api.controller.order.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +12,6 @@ public class CreateOrderRequest {
     @NotBlank(message = "주소는 필수입니다.")
     private String address;
 
-    @Min(value = 1, message = "주문 상품 수량은 1 이상이어야 합니다.")
+    @Positive(message = "주문 상품 수량은 1 이상이어야 합니다.")
     private int orderGoodsQuantity;
 }
