5313959796adf825aed364809b678569a997d410
woodongle <315ehdgur@gmail.com>
2025-08-24 14:10:21 +0900

    feat: UserService에서 사용할 request dto 생성
    1. Controller 계층과 Service 계층에서 사용하는 request를 분리하기 위해 Service용 request dto 생성

---
+++ src/main/java/mini/minishop/api/service/user/request/CreateUserServiceRequest.java
@@ -0,0 +1,28 @@
+package mini.minishop.api.service.user.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateUserServiceRequest {
+
+    @NotBlank(message = "이름은 필수입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+
+    @Builder
+    public CreateUserServiceRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    
+}
