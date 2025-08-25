f9f6e04c9784b7cf1758cfde7f7bf28448dd3684
woodongle <315ehdgur@gmail.com>
2025-08-25 13:19:07 +0900

    refactor: Service 계층용 request dto의 필드 검증 코드 제거
    1. Service 계층에서 사용하지 않는 필드 검증 코드 제거

--- src/main/java/mini/minishop/api/service/user/request/CreateUserServiceRequest.java
+++ src/main/java/mini/minishop/api/service/user/request/CreateUserServiceRequest.java
@@ -1,6 +1,5 @@
 package mini.minishop.api.service.user.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,13 +9,8 @@ import mini.minishop.domain.user.User;
 @Setter
 public class CreateUserServiceRequest {
 
-    @NotBlank(message = "이름은 필수입니다.")
     private String name;
-
-    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
-
-    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 
     @Builder
