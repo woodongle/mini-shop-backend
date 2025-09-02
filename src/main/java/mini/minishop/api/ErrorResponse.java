bfd414b0a73042499951c8ed01ca3ce3a6629835
woodongle <315ehdgur@gmail.com>
2025-09-02 14:48:10 +0900

    feat: 예외 응답 생성 로직 추가
    1. Controller에서 사용자 입력에 대한 유효성 검사가 실패할 때 발생하는 메시지를 동적으로 받을 수 있게 예외 응답 생성 로직 추가

--- src/main/java/mini/minishop/api/ErrorResponse.java
+++ src/main/java/mini/minishop/api/ErrorResponse.java
@@ -15,4 +15,8 @@ public class ErrorResponse {
     public static ErrorResponse of(ErrorCode errorCode) {
         return new ErrorResponse(errorCode.getStatus().value(), errorCode.getStatus(), errorCode.getMessage());
     }
+
+    public static ErrorResponse of(ErrorCode errorCode, String message) {
+        return new ErrorResponse(errorCode.getStatus().value(), errorCode.getStatus(), message);
+    }
 }
