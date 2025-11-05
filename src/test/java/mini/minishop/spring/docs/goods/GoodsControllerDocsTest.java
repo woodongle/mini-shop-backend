264487b2f0c9bb59a367b5a8745bae10fd6eca53
woodongle <315ehdgur@gmail.com>
2025-11-05 16:12:22 +0900

    feat: Spring REST Docs 설정 추가, 상품 등록 API 문서화
    1. Spring REST Docs를 사용하면서 만들어지는 adoc 파일을 include해서 관리할 수 있도록 설정을 추가한다.
    2. 상품 등록 API를 문서화한다.

---
+++ src/test/java/mini/minishop/spring/docs/goods/GoodsControllerDocsTest.java
@@ -0,0 +1,104 @@
+package mini.minishop.spring.docs.goods;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseBody;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import mini.minishop.api.controller.goods.GoodsController;
+import mini.minishop.api.controller.goods.request.CreateGoodsRequest;
+import mini.minishop.api.service.goods.GoodsService;
+import mini.minishop.api.service.goods.request.CreateGoodsServiceRequest;
+import mini.minishop.api.service.user.UserService;
+import mini.minishop.config.JpaAuditingConfig;
+import mini.minishop.domain.goods.Goods;
+import mini.minishop.domain.user.User;
+import mini.minishop.domain.user.UserRole;
+import mini.minishop.spring.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@WebMvcTest(
+        value = GoodsController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = JpaAuditingConfig.class
+                )
+        })
+public class GoodsControllerDocsTest extends RestDocsSupport {
+
+    @MockitoBean
+    private GoodsService goodsService;
+
+    @MockitoBean
+    private UserService userService;
+
+    @DisplayName("신규 상품을 등록하는 API")
+    @WithMockUser(username = "user@user.com", roles = "USER")
+    @Test
+    void createGoods() throws Exception {
+        User mockUser = User.builder()
+                .email("user@user.com")
+                .role(UserRole.USER)
+                .build();
+
+        given(userService.findUser(mockUser.getEmail())).willReturn(mockUser);
+
+        CreateGoodsRequest request = CreateGoodsRequest.builder()
+                .name("goods")
+                .price(new BigDecimal("1000.00"))
+                .inventoryQuantity(100)
+                .build();
+
+        given(goodsService.createGoods(any(CreateGoodsServiceRequest.class), eq(mockUser)))
+                .willReturn(Goods.builder()
+                        .name(request.getName())
+                        .price(request.getPrice())
+                        .inventoryQuantity(request.getInventoryQuantity())
+                        .user(mockUser)
+                        .build());
+
+        mockMvc.perform(post("/api/v1/goods")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().string("상품 등록이 완료되었습니다."))
+                .andDo(document("goods-create",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING)
+                                        .description("상품 이름"),
+                                fieldWithPath("price").type(JsonFieldType.NUMBER)
+                                        .description("상품 가격"),
+                                fieldWithPath("inventoryQuantity").type(JsonFieldType.NUMBER)
+                                        .description("상품 재고 수량")
+                        ),
+                        responseBody()
+                ));
+
+    }
+}
