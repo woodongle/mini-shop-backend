ab6f08f6d4a21251af25f8c037987c212c078747
woodongle <315ehdgur@gmail.com>
2025-11-06 18:15:39 +0900

    feat: 빌더 패턴 적용
    1. Spring REST Docs로 API를 문서화 할 때 쓸데없는 생성자를 만들지 않도록 하기 위해 빌더 패턴을 적용한다.

--- src/main/java/mini/minishop/api/service/goods/response/UpdateGoodsResponse.java
+++ src/main/java/mini/minishop/api/service/goods/response/UpdateGoodsResponse.java
@@ -2,6 +2,7 @@ package mini.minishop.api.service.goods.response;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import mini.minishop.domain.goods.Goods;
@@ -21,5 +22,15 @@ public class UpdateGoodsResponse {
         this.price = goods.getPrice();
         this.inventoryQuantity = goods.getInventoryQuantity();
         this.modifiedDate = goods.getModifiedDate();
+    }
+
+    @Builder
+    public UpdateGoodsResponse(Long goodsId, String goodsName, BigDecimal price, int inventoryQuantity,
+                               LocalDateTime modifiedDate) {
+        this.goodsId = goodsId;
+        this.goodsName = goodsName;
+        this.price = price;
+        this.inventoryQuantity = inventoryQuantity;
+        this.modifiedDate = modifiedDate;
     }
 }
