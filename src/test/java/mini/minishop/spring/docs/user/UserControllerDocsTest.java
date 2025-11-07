cb334bf80a89087661439a52f72051ead0c2d515
woodongle <315ehdgur@gmail.com>
2025-11-07 11:32:55 +0900

    feat: 회원을 등록하는 API 문서화
    1. 회원을 등록하는 API를 Spring REST Docs를 사용하여 문서화한다.

---
+++ src/test/java/mini/minishop/spring/docs/user/UserControllerDocsTest.java
@@ -0,0 +1,100 @@
+package mini.minishop.spring.docs.user;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseBody;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import mini.minishop.api.controller.user.UserController;
+import mini.minishop.api.controller.user.request.CreateUserRequest;
+import mini.minishop.api.service.user.RefreshTokenService;
+import mini.minishop.api.service.user.UserService;
+import mini.minishop.config.JpaAuditingConfig;
+import mini.minishop.config.JwtTokenProvider;
+import mini.minishop.domain.user.User;
+import mini.minishop.spring.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@WebMvcTest(
+        value = UserController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = JpaAuditingConfig.class
+                )
+        })
+public class UserControllerDocsTest extends RestDocsSupport {
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @MockitoBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockitoBean
+    private RefreshTokenService refreshTokenService;
+
+    @DisplayName("회원을 등록하는 API")
+    @WithMockUser(username = "user@user.com", roles = "USER")
+    @Test
+    void createUser() throws Exception {
+        CreateUserRequest request = CreateUserRequest.builder()
+                .name("user")
+                .email("user@user.com")
+                .password("user")
+                .build();
+
+        given(userService.createUser(any()))
+                .willReturn(User.builder()
+                        .name(request.getName())
+                        .email(request.getEmail())
+                        .password(request.getPassword())
+                        .build());
+
+        mockMvc.perform(post("/api/v1/users/signup")
+                        .with(csrf())
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code").value("201"))
+                .andExpect(jsonPath("$.status").value("CREATED"))
+                .andExpect(jsonPath("$.message").value("가입을 축하드립니다. 로그인 화면으로 이동합니다."))
+                .andDo(document("user/user-create",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("name").type(STRING)
+                                        .description("사용자 이름"),
+                                fieldWithPath("email").type(STRING)
+                                        .description("사용자 이메일"),
+                                fieldWithPath("password").type(STRING)
+                                        .description("비밀번호")
+                        ),
+                        responseBody()
+                ));
+    }
+}
