a4393fea524c7318cefec9be365846034ed08cfc
woodongle <315ehdgur@gmail.com>
2025-11-08 16:43:17 +0900

    feat: 주문을 취소하는 API 문서화
    1. 주문을 취소하는 API를 Spring REST Docs를 사용하여 문서화한다.

--- src/test/java/mini/minishop/spring/docs/order/OrderControllerDocsTest.java
+++ src/test/java/mini/minishop/spring/docs/order/OrderControllerDocsTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.mock;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
@@ -29,6 +30,7 @@ import mini.minishop.api.controller.order.OrderController;
 import mini.minishop.api.controller.order.request.CreateOrderRequest;
 import mini.minishop.api.service.order.OrderService;
 import mini.minishop.api.service.order.request.CreateOrderServiceRequest;
+import mini.minishop.api.service.order.response.CancelOrderResponse;
 import mini.minishop.api.service.order.response.CreateOrderResponse;
 import mini.minishop.api.service.order.response.FindOrderHistoryResponse;
 import mini.minishop.api.service.order.response.OrderGoodsResponse;
@@ -215,6 +217,77 @@ public class OrderControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("data[].orderGoods[].quantity").type(NUMBER)
                                         .description("상품 수량"),
                                 fieldWithPath("data[].orderGoods[].paymentAmount").type(NUMBER)
+                                        .description("총 상품 가격")
+                        )
+                ));
+    }
+
+    @DisplayName("사용자 ID로 주문을 취소하는 API")
+    @WithMockUser(username = "user@user.com", roles = "USER")
+    @Test
+    void cancelOrder() throws Exception {
+        Long mockUserId = 1L;
+        String mockUserEmail = "user@user.com";
+        Long mockOrderId = 1L;
+
+        OrderGoodsResponse goods1 = OrderGoodsResponse.builder()
+                .goodsName("상품1")
+                .quantity(1)
+                .paymentAmount(new BigDecimal("2000.00"))
+                .build();
+        OrderGoodsResponse goods2 = OrderGoodsResponse.builder()
+                .goodsName("상품2")
+                .quantity(2)
+                .paymentAmount(new BigDecimal("5000.00"))
+                .build();
+        CancelOrderResponse response = CancelOrderResponse.builder()
+                .orderId(mockUserId)
+                .orderStatus(OrderStatus.CANCELED_ORDER)
+                .orderedDate(LocalDateTime.of(2000, 1, 1, 9, 0))
+                .canceledDate(LocalDateTime.of(2000, 1, 1, 10, 0))
+                .orderGoods(List.of(goods1, goods2))
+                .build();
+
+        User user = mock(User.class);
+
+        given(user.getId()).willReturn(mockUserId);
+        given(userService.findUser(mockUserEmail))
+                .willReturn(user);
+        given(orderService.cancelOrder(mockOrderId, mockUserId))
+                .willReturn(response);
+
+        mockMvc.perform(patch("/api/v1/order/{orderId}/cancel", mockOrderId)
+                        .with(csrf())
+                        .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andDo(document("/order/order-cancel",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("code").type(NUMBER)
+                                        .description("응답 코드"),
+                                fieldWithPath("status").type(STRING)
+                                        .description("응답 상태"),
+                                fieldWithPath("message").type(STRING)
+                                        .description("응답 메시지"),
+                                fieldWithPath("data").type(OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.orderId").type(NUMBER)
+                                        .description("주문 ID"),
+                                fieldWithPath("data.orderStatus").type(STRING)
+                                        .description("주문 상태"),
+                                fieldWithPath("data.orderedDate").type(STRING)
+                                        .description("주문 날짜"),
+                                fieldWithPath("data.canceledDate").type(STRING)
+                                        .description("주문 취소 날짜"),
+                                fieldWithPath("data.orderGoods[].goodsName").type(STRING)
+                                        .description("상품 이름"),
+                                fieldWithPath("data.orderGoods[].quantity").type(NUMBER)
+                                        .description("상품 수량"),
+                                fieldWithPath("data.orderGoods[].paymentAmount").type(NUMBER)
                                         .description("총 상품 가격")
                         )
                 ));
