38860d7143442bf7ced4162552260573a0817f5d
woodongle <315ehdgur@gmail.com>
2025-11-12 16:31:56 +0900

    fix: 에러 반환 로직 수정
    1. 이전에 있던 로직으로 되돌린 후 필요한 부분만 간단하게 수정한다.

--- src/main/java/mini/minishop/api/ApiControllerAdvice.java
+++ src/main/java/mini/minishop/api/ApiControllerAdvice.java
@@ -3,31 +3,35 @@ package mini.minishop.api;
 import mini.minishop.exception.BusinessException;
 import mini.minishop.exception.CommonErrorCode;
 import mini.minishop.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiControllerAdvice {
 
     @ExceptionHandler(BusinessException.class)
-    protected ApiResponse<ErrorResponse> handleBusinessException(BusinessException e) {
+    protected ResponseEntity<ApiResponse<ErrorResponse>> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = ErrorResponse.of(errorCode);
 
-        return ApiResponse.of(response.getStatus(), response);
+        return new ResponseEntity<>(ApiResponse.of(response.getStatus(), response), errorCode.getStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ApiResponse<ErrorResponse> handleMethodArgumentNotValidException(
+    protected ResponseEntity<ApiResponse<ErrorResponse>> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException e) {
         String message = e.getBindingResult().getFieldErrors().getFirst().getDefaultMessage();
         ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
         ErrorResponse response = ErrorResponse.of(errorCode, message);
 
-        return ApiResponse.of(response.getStatus(), response);
+        return new ResponseEntity<>(ApiResponse.of(response.getStatus(), response), errorCode.getStatus());
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     protected ApiResponse<ErrorResponse> handleException(Exception e) {
         ErrorResponse response = ErrorResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR);
