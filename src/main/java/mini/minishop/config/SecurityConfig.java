97d527d8940bd432734c6580758d4fd0f1985d56
woodongle <315ehdgur@gmail.com>
2025-11-14 17:28:02 +0900

    feat: 프로젝트 성능 모니터링을 위한 설정 추가
    1. 프로젝트의 성능을 모니터링하기 위해 설정 추가
      - prometheus(데이터 수집)
      - grafana(데이터 시각화)

--- src/main/java/mini/minishop/config/SecurityConfig.java
+++ src/main/java/mini/minishop/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/users/refresh", "/api/v1/users/signup", "/api/v1/users/login",
-                                "/h2-console/**").permitAll()
+                                "/h2-console/**", "/actuator/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
