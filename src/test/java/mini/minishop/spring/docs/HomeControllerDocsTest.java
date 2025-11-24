fdef0d2102112b9ccd747f5d06df28e2dc2e70ed
woodongle <315ehdgur@gmail.com>
2025-11-24 15:28:18 +0900

    feat: 서비스의 메인 페이지 추가 후 API 문서 추가
    1. 서비스의 메인 페이지 추가 후 API 문서를 추가한다.

---
+++ src/test/java/mini/minishop/spring/docs/HomeControllerDocsTest.java
@@ -0,0 +1,54 @@
+package mini.minishop.spring.docs;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import mini.minishop.api.controller.HomeController;
+import mini.minishop.config.JpaAuditingConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+
+@WebMvcTest(
+        value = HomeController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = JpaAuditingConfig.class
+                )
+        })
+public class HomeControllerDocsTest extends RestDocsSupport {
+
+    @DisplayName("메인 페이지 API")
+    @WithMockUser(username = "user@user.com", roles = "USER")
+    @Test
+    void home() throws Exception {
+        mockMvc.perform(get("/")
+                        .contentType(APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andDo(document("home",
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("status").description("응답 상태"),
+                                fieldWithPath("message").description("응답 메시지"),
+                                fieldWithPath("data").description("응답 데이터"),
+                                fieldWithPath("code").description("응답 코드")
+                        )
+                ));
+    }
+}
