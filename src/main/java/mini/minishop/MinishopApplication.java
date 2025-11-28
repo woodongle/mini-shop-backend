c82675b3cbebc3e12e785e4f2ff6691fd31c3d42
woodongle <315ehdgur@gmail.com>
2025-11-28 14:17:48 +0900

    chore: test deploy

--- src/main/java/mini/minishop/MinishopApplication.java
+++ src/main/java/mini/minishop/MinishopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MinishopApplication {
 
     public static void main(String[] args) {
-        System.out.println("Minishop 애플리케이션 시작!!");
+        System.out.println("Minishop 애플리케이션 시작!!!");
         SpringApplication.run(MinishopApplication.class, args);
     }
 }
